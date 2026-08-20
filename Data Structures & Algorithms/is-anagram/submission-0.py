fad4b5075084d0535936c006ddd82f643af3536b
Yerassyl Sailaubay <erasyl.200614@gmail.com>
2026-08-20 14:58:40 +0500

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        t_dict = {}
+        for ch in s:
+            if ch not in s_dict:
+                s_dict[ch] = 1
+            s_dict[ch] += 1
+                
+        for ch in t:
+            if ch not in t_dict:
+                t_dict[ch] = 1
+            t_dict[ch] += 1
+
+        if s_dict == t_dict:
+            return True
+
+        else:
+            return False
+
+        
